1653fbf2b819fcb5cb8c94fe8a11ae2f798b98e0
Tufan <62504339+TemeT-sketch@users.noreply.github.com>
2021-03-08 19:47:42 +0300

    Add files via upload

---
+++ LoginApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            int hak = 3;
+            while (kontrol)
+            {
+                Console.WriteLine("Kullanıcı Adınızı giriniz");
+                string isim = Console.ReadLine();
+
+                Console.WriteLine("şifrenizi giriniz");
+                string parola = Console.ReadLine();
+
+                if (isim == "admin" && parola == "admin")
+                {
+                    Console.WriteLine("Başarılı bir şekilde giriş yaptınız...");
+                    Console.ReadLine();
+                    break;
+                }
+
+                if (hak <= 0)
+                {
+                    Console.WriteLine("Üçten fazla hatalı giriş yaptınız tekrar deneyiniz.");
+                    Console.ReadLine();
+                    kontrol = false;
+                }
+                else
+                {
+                    hak -= 1;
+                }
+            }
+        }
+    }
+}
